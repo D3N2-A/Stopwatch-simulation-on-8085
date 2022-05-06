5d54255e95185b4b7625b73b014b2f41e092c56a
D3N2-A <anmolsharma999937@gmail.com>
2022-05-06 23:19:24 +0530

    Delay of 1ms subroutine

--- stopwatch.asm
+++ stopwatch.asm
@@ -8,3 +8,15 @@ CALL UPDAD; Display time present in HL in the address field
 MVI A, 00011011B
 SIM ; Unmask RST7.5, Reset RST7.5 flip flop
 EI ; Enable interrupt system
+CALL DELAY ; Generate a delay of 1 second
+DELAY: MVI B, 02H
+OUTLOOP: LXI D, FFFFH
+
+INLOOP: DCX D
+MOV A, D
+ORA E
+JNZ INLOOP
+
+DCR B
+JNZ OUTLOOP
+RET
