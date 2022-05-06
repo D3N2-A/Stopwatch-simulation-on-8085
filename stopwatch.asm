cd403516f8d0f88a45651e8e5be7dc5fd4eeeeac
D3N2-A <anmolsharma999937@gmail.com>
2022-05-06 23:34:14 +0530

    rollover after counting to 60

--- stopwatch.asm
+++ stopwatch.asm
@@ -9,6 +9,7 @@ MVI A, 00011011B
 SIM ; Unmask RST7.5, Reset RST7.5 flip flop
 EI ; Enable interrupt system
 CALL DELAY ; Generate a delay of 1 second
+; -------Delay subroutine start-----------
 DELAY: MVI B, 02H
 OUTLOOP: LXI D, FFFFH
 
@@ -20,3 +21,10 @@ JNZ INLOOP
 DCR B
 JNZ OUTLOOP
 RET
+; -------Delay subroutine end-----------
+LHLD CURAD
+MOV A, L
+ADI 01H
+DAA ; Increment L value in decimal
+CPI 60H
+JZ INC_MIN ; If L = 60, jump to INC_MIN
