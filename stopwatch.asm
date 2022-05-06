6cc7a98831b04e4ea9ff50955db184be57fc4f68
D3N2-A <anmolsharma999937@gmail.com>
2022-05-06 23:15:03 +0530

    Unmasking 7.5 and enabling interrupt system

--- stopwatch.asm
+++ stopwatch.asm
@@ -5,3 +5,6 @@ UPDAD: EQU 06BCH; Assign UPDAD to 06BCH
 RESET: LXI H,0000H
 REPEAT: SHLD CURAD
 CALL UPDAD; Display time present in HL in the address field
+MVI A, 00011011B
+SIM ; Unmask RST7.5, Reset RST7.5 flip flop
+EI ; Enable interrupt system
